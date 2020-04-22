9c438ee7950a3da2e9a9bf9ddd098fd0bbdf3e40
Zachary Smeton <zsmeton@yahoo.com>
2020-04-21 18:48:44 -0600

    Final version of this branch

--- main.cpp
+++ main.cpp
@@ -1113,7 +1113,7 @@ void setupSDFs() {
     modelLoader->setTriangleLocation(sdfSSBOLocs.triangles);
 
     // Calculate SDF
-    modelLoaderMtx = glm::translate(glm::mat4(1.0), glm::vec3(0.0, -4.1, 0.0));
+    modelLoaderMtx = glm::translate(glm::mat4(1.0), glm::vec3(0.0, -4.0, 0.0));
     //modelLoader->calculateSignedDistanceFieldCPU(SDF_RESOLUTION, SDF_OFFSET, modelLoaderMtx);
     modelLoader->calculateSignedDistanceField(sdfProgram, SDF_RESOLUTION, SDF_OFFSET, modelLoaderMtx);
 }
@@ -1569,17 +1569,27 @@ void renderScene(GLFWwindow *window) {
     diffuse.r = matReader.getSwatch(FLOOR_MATERIAL).diffuse[0];
     diffuse.g = matReader.getSwatch(FLOOR_MATERIAL).diffuse[1];
     diffuse.b = matReader.getSwatch(FLOOR_MATERIAL).diffuse[2];
-    diffuse.a = 0.9;
+    diffuse.a = 1.0;
+#if SDF_DEBUG
+    diffuse.a = 0.4;
+#endif
     glm::vec4 ambient;
     ambient.r = matReader.getSwatch(FLOOR_MATERIAL).ambient[0];
     ambient.g = matReader.getSwatch(FLOOR_MATERIAL).ambient[1];
     ambient.b = matReader.getSwatch(FLOOR_MATERIAL).ambient[2];
-    ambient.a = 0.9;
+    ambient.a = 1.0;
+#if SDF_DEBUG
+    ambient.a = 0.4;
+#endif
     glm::vec4 specular;
     specular.r = matReader.getSwatch(FLOOR_MATERIAL).specular[0];
     specular.g = matReader.getSwatch(FLOOR_MATERIAL).specular[1];
     specular.b = matReader.getSwatch(FLOOR_MATERIAL).specular[2];
-    specular.a = 0.9;
+    specular.a = 1.0;
+#if SDF_DEBUG
+    specular.a = 0.4;
+#endif
+
     glBufferSubData(GL_UNIFORM_BUFFER, materialUniformBuffer.offsets[0], sizeof(float) * 4, &(diffuse)[0]);
     glBufferSubData(GL_UNIFORM_BUFFER, materialUniformBuffer.offsets[1], sizeof(float) * 4, &(specular)[0]);
     glBufferSubData(GL_UNIFORM_BUFFER, materialUniformBuffer.offsets[2], sizeof(float),
